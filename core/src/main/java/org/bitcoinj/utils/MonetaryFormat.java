5296394bf8ebce2741fc0b2dd2f494d7efb77c3b
HashEngineering <hashengineeringsolutions@gmail.com>
2022-06-02 21:28:47 -0700

    fix: update MonetaryFormat codes for GRS

--- core/src/main/java/org/bitcoinj/utils/MonetaryFormat.java
+++ core/src/main/java/org/bitcoinj/utils/MonetaryFormat.java
@@ -58,11 +58,11 @@ public final class MonetaryFormat {
     /** Standard format for fiat amounts. */
     public static final MonetaryFormat FIAT = new MonetaryFormat().shift(0).minDecimals(2).repeatOptionalDecimals(2, 1);
     /** Currency code for base 1 Bitcoin. */
-    public static final String CODE_BTC = CoinDefinition.coinTicker;//"BTC";
+    public static final String CODE_BTC = "GRS";
     /** Currency code for base 1/1000 Bitcoin. */
-    public static final String CODE_MBTC = "m"+CoinDefinition.coinTicker;//"mBTC";
+    public static final String CODE_MBTC = "mGRS";
     /** Currency code for base 1/1000000 Bitcoin. */
-    public static final String CODE_UBTC = "µGRS"; //"µBTC";
+    public static final String CODE_UBTC = "µGRS";
     /** Currency symbol for base 1 Bitcoin. */
     /** Currency code for base 1 satoshi. */
     public static final String CODE_SAT = "gro";
