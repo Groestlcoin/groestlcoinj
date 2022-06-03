d56f75ef3ca50ad00b2a6f3b5f8fcd57a266c767
HashEngineering <hashengineeringsolutions@gmail.com>
2022-06-02 21:30:49 -0700

    test: fix TransactionOutputTest

--- core/src/test/java/org/bitcoinj/core/TransactionOutputTest.java
+++ core/src/test/java/org/bitcoinj/core/TransactionOutputTest.java
@@ -87,10 +87,10 @@ public class TransactionOutputTest extends TestWithWallet {
     @Test
     public void getMinNonDustValue() throws Exception {
         TransactionOutput p2pk = new TransactionOutput(UNITTEST, null, Coin.COIN, myKey);
-        assertEquals(Coin.valueOf(576), p2pk.getMinNonDustValue());
+        assertEquals(Coin.valueOf(5760), p2pk.getMinNonDustValue());
         TransactionOutput p2pkh = new TransactionOutput(UNITTEST, null, Coin.COIN, LegacyAddress.fromKey(UNITTEST, myKey));
-        assertEquals(Coin.valueOf(546), p2pkh.getMinNonDustValue());
+        assertEquals(Coin.valueOf(5460), p2pkh.getMinNonDustValue());
         TransactionOutput p2wpkh = new TransactionOutput(UNITTEST, null, Coin.COIN, SegwitAddress.fromKey(UNITTEST, myKey));
-        assertEquals(Coin.valueOf(294), p2wpkh.getMinNonDustValue());
+        assertEquals(Coin.valueOf(2940), p2wpkh.getMinNonDustValue());
     }
 }
